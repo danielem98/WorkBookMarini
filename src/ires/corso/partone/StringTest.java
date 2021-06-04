fc712c3abb121c1f359c0de120a83ae51874b5f9
danielem98 <marini.daniele98@gmail.com>
2021-06-04 17:20:29 +0200

    Initial commit

---
+++ src/ires/corso/partone/StringTest.java
@@ -0,0 +1,9 @@
+package ires.corso.partone;
+
+public class StringTest {
+    //scrivere un programma in java che:
+    //1. accetta in input 3 stringhe (da riga di comando)
+    public static void main(String[] args){
+
+    }
+}
